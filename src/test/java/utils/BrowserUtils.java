bd60d3aeffe53b561bc1670d82cbe1e5caf12619
Sanjar Sharipov <Sanjarty@gmail.com>
2019-10-24 22:02:44 -0400

    "MyFirstProject"

---
+++ src/test/java/utils/BrowserUtils.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class BrowserUtils {
+    public static void wait(int second) {
+        try {
+            Thread.sleep(1000 * second);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
